c2bc21899d8073a1361b55c2319ddbea4bb1c965
Ian Booton <ianbooton@Ians-MacBook-Pro.local>
2017-05-24 15:09:40 -0400

    modals are broken

---
+++ js/scripts.js
@@ -0,0 +1,67 @@
+// Wait until the DOM is loaded...
+function start(){
+	$(document).ready(function(){
+		// console.log("test")
+		// All api calls go to the this link
+		const apiBaseUrl = 'http://api.themoviedb.org/3';
+		// All images use this link
+		const imageBaseUrl = 'http://image.tmdb.org/t/p/';
+
+		const nowPlayingUrl = apiBaseUrl + '/movie/now_playing?api_key='+apiKey
+		// console.log(nowPlayingUrl);
+
+		// Make AJAX request to the nowPlayingUrl
+		console.log(nowPlayingUrl)
+		$.getJSON(nowPlayingUrl,(nowPlayingData)=>{
+			// console.log(nowPlayingData);
+			var nowPlayingHTML = getHTML(nowPlayingData);
+			$('#movie-grid').html(nowPlayingHTML);
+			$('.movie-poster').click(()=>{
+				// Change teh HTML inside the modal
+				var thisMovieId = $(this).attr('movie-id');
+				console.log(thisMovieId);
+				var thisMovieUrl = `${apiBaseUrl}/movie/${thisMovieId}?api_key=${apiKey}`;
+				$.getJSON(thisMovieUrl,(thisMovieData)=>{
+					console.log(thisMovieData);
+					$('#myModalLabel').html(thisMovieData.title);
+					$('.modal-body').html(thisMovieData.overview);
+					// Open teh modal
+					$("#myModal").modal();
+				});
+			});
+		});
+
+		$('#movie-form').submit((event)=>{
+			// Dont submit form! JS will handle
+			event.preventDefault();
+			var userInput = $('#search-input').val();
+			$('#search-input').val('');
+			var safeUserInput = encodeURI(userInput);
+			var searchUrl = apiBaseUrl + '/search/movie?query='+safeUserInput+'&api_key='+apiKey;
+			// console.log(searchUrl);
+			$.getJSON(searchUrl,(searchMovieData)=>{
+				var searchMovieHTML = getHTML(searchMovieData);
+				$('#movie-grid').html(searchMovieHTML);
+			})
+		})
+
+		function getHTML(data){
+			var newHTML = '';
+			for(let i = 0; i < data.results.length; i++){
+				var posterUrl = imageBaseUrl + 'w300' + data.results[i].poster_path;
+				newHTML += '<div class="col-sm-6 col-md-3 movie-poster" movie-id='+data.results[i].id+'>';
+					newHTML += `<img src="${posterUrl}">`;
+				newHTML += `</div>`;
+			}
+			return newHTML;
+		}
+
+		$('.home-button').click(()=>{
+			// alert('Home button clicked!')
+			start()
+		})
+
+	});
+}
+start()
+
